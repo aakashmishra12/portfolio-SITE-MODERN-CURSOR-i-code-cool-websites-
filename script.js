77b4ec5ad4436464404f87814676a12f64197614
aakash Mishra <aakashmishra9545382296@gmail.com>
2023-07-31 12:59:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function openNav() {
+    document.getElementById("mySidenav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.height = "0";
+}
+
+
+var inc = 1000;
+clock();
+
+
+
+
+
+
+
+var inc = 1000;
+clock();
+
+function clock() {
+    const date = new Date();
+    const hours = ((date.getHours() + 11) % 12 + 1);
+    const minutes = date.getMinutes();
+    const seconds = date.setSeconds();
+    const hour = hours * 30;
+    const minute = minutes * 6;
+    const second = seconds * 6;
+    const h = (hours < 10) ? "0" + hours : hours;
+    const m = (minutes < 10) ? "0" + minutes : minutes;
+    document.querySelector('.hours').innerHTML = h;
+    document.querySelector('.mins').innerHTML = m;
+    document.querySelector('.hour').style.transform = `rotate(${hour}deg)`
+    document.querySelector('.minute').style.transform = `rotate(${minute}deg)`
+    document.querySelector('.second').style.transform = `rotate(${second}deg)`
+
+}
+
+setInterval(clock, inc);
